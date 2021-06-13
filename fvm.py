03a8d86750c3315d4db154efe528bbd3c1d85656
Yahya <yahyaansari62@gmail.com>
2021-06-13 22:36:09 +0530

    feat graph

--- fvm.py
+++ fvm.py
@@ -1,30 +1,46 @@
+'''   Copyright 2021 MOHAMMED YAHYA ANSARI
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+'''
 import pandas as pd
+import matplotlib.pyplot as plt
+
 print("\n\t\t Finite Volume Method for Steady State Diffusion \n")
 
-#Switch case for type of numerical
+# Switch case for type of numerical
 choice = ""
-while choice !="q":
+while choice != "q":
     print("""\t[ 1 ] Diffusion without Source
 
         [ 2 ] Diffusion with source
 
         [ q ] Exit\n""")
-    choice = input("\n\tEnter Choice :\t") 
+    choice = input("\n\tEnter Choice :\t")
 
     if choice == "1":
-        print("Diffusion Withour Source")
+        print("\nDiffusion Withour Source")
         q = 0
         break
     elif choice == "2":
-        print("Diffusion with Source")
+        print("\nDiffusion with Source")
         q = float(input("\n\tEnter uniform heat generation q in W/m2:   "))
         break
     elif choice == "q":
         exit()
-    else :print("\n\n\tInvalid choice, Try again!")
+    else:
+        print("\n\n\tInvalid choice, Try again!\n")
 
-
-#input from user
+# input from user
 n = int(input("\n\tEnter the number of grid points:   "))
 l = float(input("\n\tEnter length of plate in m:   "))
 tk = float(input("\n\tEnter thermal conductivity of plate in W/mK or W/mC:   "))
@@ -44,6 +60,7 @@ Err = [0]*n
 Texact = [0]*n
 g = [0]*n
 
+# setting up equations in tdma format
 dx = l/n
 D[0] = (3*tk)/dx
 D[1] = (2*tk)/dx
@@ -51,7 +68,6 @@ D[n-1] = (3*tk)/dx
 beta[1] = tk/dx
 alpha[1] = tk/dx
 c[0] = ((2*tk*ta)/dx)+(q*dx)
-
 
 for i in range(1, n-1):
     c[i] = q*dx
@@ -62,57 +78,77 @@ beta[n-1] = beta[1]
 alpha[0] = alpha[1]
 alpha[n-1] = 0
 
-# add common value to list D beta and alpha
+# add common value to list D, beta and alpha
 for i in range(2, n-1):
     D[i] = D[1]
     beta[i] = beta[1]
     alpha[i] = alpha[1]
 
-print("\n")
-
-# for i in range(0, n):
-#     print("\t D%d is %f \t Beta %d is %f \t Alpha%d is %f \t C%d is %f" %
-#           (i+1, D[i], i+1, beta[i], i+1, alpha[i], i+1, c[i]))
-#     print("\n")
-
-a = alpha[0]
-A[0] = a/D[0]
-C[0] = c[0]/D[0]
-for i in range(1, n):
+# Calculating intermediate terms by forward substitution
+for i in range(0, n):
     A[i] = alpha[i]/(D[i] - beta[i]*A[i-1])
     C[i] = (beta[i]*C[i-1] + c[i])/(D[i] - beta[i]*A[i-1])
 
-# for i in range(0, n):
-#     print("\tA%d = %f \t\t C'%d = %f" % (i+1, A[i], i+1, C[i]))
-#     print("\n")
-# print("\n")
-
-# Calculating Temperarure values
+# equating last value for back substitution
 temp[n-1] = C[n-1]
+# Calculating Temperarure values by backward substitution
 j = n-2
 while j >= 0:
     temp[j] = A[j] * temp[j+1] + C[j]
     j = j-1
 
-# print("Temperature values are:\n")
-# for i in range(0, n):
-#     print("\t\t T%d = %f C \n" % (i+1, temp[i]))
-
-
-#Exact Solution and error
+# Calculating Exact Solution and error
 A1 = (-q)/(2*tk)
 A2 = tb/l - ta/l + (q*l)/(2*tk)
 dx = l/n
-for i in range (0,n):
-    g[i] = dx*0.5 + ( dx *i)
-    Texact[i] = A1 * g[i] * g[i] + A2 * g[i] + ta
-    Err[i] = ((temp[i] - Texact[i]) *100*2)/ (temp[i] + Texact[i])
 
-OUTPUT = list(zip(beta , D , alpha , c , A , C , temp, Texact, Err))
-#create Pandas DataFrame
-result = pd.DataFrame(data = OUTPUT, columns = ["\N{GREEK SMALL LETTER BETA}","Diagonal(D)","\N{GREEK SMALL LETTER ALPHA}","Constant(C)","A","C'","Temperature(T)","Temperature Exact(T exact)","% Error"])
-#change index to 1,2,3,.....
+for i in range(0, n):
+    g[i] = dx*0.5 + (dx * i)
+    Texact[i] = A1 * g[i] * g[i] + A2 * g[i] + ta
+    Err[i] = ((temp[i] - Texact[i]) * 100*2) / (temp[i] + Texact[i])
+
+# create output tuple
+OUTPUT = list(zip(beta, D, alpha, c, A, C, temp, Texact, Err))
+
+# create Pandas DataFrame
+result = pd.DataFrame(data=OUTPUT, columns=["\N{GREEK SMALL LETTER BETA}", "Diagonal(D)", "\N{GREEK SMALL LETTER ALPHA}",
+                      "Constant(C)", "A", "C'", "Temperature(T)", "Temperature Exact(T exact)", "% Error"])
+# change index to 1,2,3,.....
 result.index = result.index + 1
 
+# print table
+print("\n\n")
 print(result)
 
+# show graph
+while choice != "q":
+    print('''\n\t[ y ] Plot graph
+        [ q ] Exit
+        ''')
+    choice = input("\nEnter yout choice :\t")
+    if choice == "y":
+        print("\nPlotting graph .........................\n")
+        # adding initial and final conditions to the list, as list contains values at nodes
+        temp.insert(0, ta)
+        # list size increased because of previous insertion so last index becomes n so we add at end n+1
+        temp.insert(n+1, tb)
+        Texact.insert(0, ta)
+        Texact.insert(n+1, tb)
+        g.insert(0, 0)
+        g.insert(n+1, l)
+
+        graph = pd.DataFrame(
+            {'Temperature': temp, 'Temperature Exact': Texact}, index=g)
+        graph.plot()
+        plt.show()
+        print("\n********** Plot Graph complete **********")
+        break
+    elif choice == "q":
+        print("\n***** Exit without graph plot *****")
+        break
+    else:
+        print("\n Invalid Choice, Try again !")
+
+
+# hold window
+input("\n\nPress Enter to Exit")
